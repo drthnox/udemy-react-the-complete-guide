e08064ea7ecb82b743484ee757ebb9f345bf9284
Dave G <46117427+drthnox@users.noreply.github.com>
2024-11-05 11:51:07 +0800

    Refactoring game board, prepping for determination of winner

--- 04-essential-deep-dive/07-tic-tac-toe-starting-project/src/App.jsx
+++ 04-essential-deep-dive/07-tic-tac-toe-starting-project/src/App.jsx
@@ -2,9 +2,17 @@ import { Player } from './components/Player.jsx'
 import { GameBoard } from './components/GameBoard.jsx';
 import { useState } from 'react';
 import { Log } from './components/Log.jsx';
+import { WINNING_COMBINATIONS } from './winning-combinations.js';
 
 const player1_symbol = 'X';
 const player2_symbol = 'O';
+
+
+const initialGameBoard = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+];
 
 
 function getCurrentPlayer(gameTurns) {
@@ -18,9 +26,25 @@ function getCurrentPlayer(gameTurns) {
 function App() {
     const [gameTurns, setGameTurns] = useState([]);
     const activePlayer = getCurrentPlayer(gameTurns);
+
+    const gameBoard = initialGameBoard;
+
+    for (const turn of gameTurns) {
+        const { square, player } = turn;
+        const { row, col } = square;
+        gameBoard[row][col] = player;
+    }
+
     let player1_active = activePlayer === player1_symbol;
     let player2_active = activePlayer === player2_symbol;
 
+    if (gameTurns.length > 2) {
+        for (const combination of WINNING_COMBINATIONS) {
+            const firstSquare = combination[0];
+            const secondSquare = combination[1];
+            const thirdSquare = combination[2];
+        }
+    }
     function handleSelectSquare(rowIndex, colIndex) {
         setGameTurns(prevTurns => {
             let currentPlayer = getCurrentPlayer(prevTurns);
@@ -40,7 +64,7 @@ function App() {
                         <Player initialName="Player 2" symbol={player2_symbol} isActive={player2_active} />
                     </li>
                 </ol>
-                <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
+                <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
             </div>
             <Log turns={gameTurns} />
         </main>
