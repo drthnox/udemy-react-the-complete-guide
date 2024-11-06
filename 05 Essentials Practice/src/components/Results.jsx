2e9b1c684a7f4f50eef2d4b8cb47ed33ec527bd7
Dave G <46117427+drthnox@users.noreply.github.com>
2024-11-06 13:34:23 +0800

    Added Results component

---
+++ 05 Essentials Practice/src/components/Results.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+export function Results() {
+    return (
+        <section id="result" className="center">
+            <h2>Results</h2>
+            <table>
+                <thead>table header</thead>
+                <tbody>table body</tbody>
+            </table>
+        </section>
+    );
+}
