2536e4db5be165144f347b98403203b3ba054238
Dave G <46117427+drthnox@users.noreply.github.com>
2024-11-30 18:11:43 +0800

    Adding name change

--- 08-Refs and Portals/src/components/Player.jsx
+++ 08-Refs and Portals/src/components/Player.jsx
@@ -1,11 +1,26 @@
+import { useState } from "react";
+
 export default function Player() {
-  return (
-    <section id="player">
-      <h2>Welcome unknown entity</h2>
-      <p>
-        <input type="text" />
-        <button>Set Name</button>
-      </p>
-    </section>
-  );
+    const [name, setName] = useState('');
+    const [nameSubmitted, setNameSubmitted] = useState(false);
+
+    function handleChange(event) {
+        let name = event.target.value;
+        setName(name);
+    }
+
+    function handleClick() {
+        setNameSubmitted(true);
+    }
+
+
+    return (
+        <section id="player">
+            <h2>Welcome {nameSubmitted ? name : 'Stranger'}</h2>
+            <p>
+                <input type="text" onChange={handleChange} value={name} />
+                <button onClick={handleClick}>Set Name</button>
+            </p>
+        </section>
+    );
 }
